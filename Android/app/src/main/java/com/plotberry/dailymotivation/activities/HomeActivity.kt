b66c3f00cdc1b8a4c9ec2fe1a83aca9d7008bc93
Plasius <Plasius@users.noreply.github.com>
2020-04-29 21:39:47 +0300

    Creating theme

--- Android/app/src/main/java/com/plotberry/dailymotivation/activities/HomeActivity.kt
+++ Android/app/src/main/java/com/plotberry/dailymotivation/activities/HomeActivity.kt
@@ -2,9 +2,12 @@ package com.plotberry.dailymotivation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.plotberry.dailymotivation.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
