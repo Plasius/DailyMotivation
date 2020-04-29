f3c2ff4e5eb1f28bb7bf972554e277275a96dcf9
dkdavid24 <dkdavid24@live.com>
2020-04-29 20:43:54 +0300

    God knows

---
+++ Android/app/src/main/java/com/plotberry/dailymotivation/activities/FavoritesActivity.kt
@@ -0,0 +1,13 @@
+package com.plotberry.dailymotivation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.plotberry.dailymotivation.R
+
+class FavoritesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
--- Android/app/src/main/java/com/plotberry/dailymotivation/activities/MainActivity.kt
+++ Android/app/src/main/java/com/plotberry/dailymotivation/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.plotberry.dailymotivation
+package com.plotberry.dailymotivation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.plotberry.dailymotivation.R
 
 class MainActivity : AppCompatActivity() {
 
