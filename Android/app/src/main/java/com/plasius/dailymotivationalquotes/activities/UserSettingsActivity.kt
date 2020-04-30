92a1195d03ae148a024ad7c87d5d2d9d214eb770
dkdavid24 <dkdavid24@live.com>
2020-04-30 22:22:05 +0300

    sign out

--- Android/app/src/main/java/com/plasius/dailymotivationalquotes/activities/UserSettingsActivity.kt
+++ Android/app/src/main/java/com/plasius/dailymotivationalquotes/activities/UserSettingsActivity.kt
@@ -1,13 +1,28 @@
 package com.plasius.dailymotivationalquotes.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import com.plasius.dailymotivationalquotes.R
 
 class UserSettingsActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_settings)
+
+        auth= FirebaseAuth.getInstance()
+    }
+
+    fun trigLogout(view: View) {
+        auth.signOut()
+        val intent= Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
