15abb9e4dbe0cfa89c268eeaad03cb69e2ae0f89
Plasius <Plasius@users.noreply.github.com>
2020-04-30 15:46:32 +0300

    Googlesignin

--- Android/app/src/main/java/com/plasius/dailymotivationalquotes/activities/LoginActivity.kt
+++ Android/app/src/main/java/com/plasius/dailymotivationalquotes/activities/LoginActivity.kt
@@ -26,8 +26,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
-
         auth = FirebaseAuth.getInstance()
     }
 
@@ -37,18 +35,18 @@ class LoginActivity : AppCompatActivity() {
             authText.text = "You have not logged in or there was an error!"
         }
         else{
-            var string_user=currentUser.email
-            authText.text = "You have logged in as: $string_user"
+            authText.text = "You have logged in as: ${currentUser.email}"
         }
 
     }
 
+    /*
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         updateUI(currentUser)
-    }
+    }*/
 
     fun trig_signuo(view: View) {
         var email = et_email.text.toString()
@@ -107,7 +105,7 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
-    public fun googleSign(view: View) {
+    fun googleSign(view: View) {
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -115,57 +113,48 @@ class LoginActivity : AppCompatActivity() {
             .requestEmail()
             .build()
 
-        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        val signInIntent = mGoogleSignInClient.signInIntent
-        startActivityForResult(signInIntent, 23)
+        startActivityForResult(mGoogleSignInClient.signInIntent, 320)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        Toast.makeText(baseContext, "Authentication in process #1.",
-            Toast.LENGTH_SHORT).show()
+
+        Toast.makeText(baseContext, "Authentication in process #1.", Toast.LENGTH_SHORT).show()
+
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == 23) {
-            Toast.makeText(baseContext, "Authentication in process #1,5.",
-                Toast.LENGTH_SHORT).show()
+        if (requestCode == 320) {
+            Toast.makeText(baseContext, "Authentication in process #1,5.", Toast.LENGTH_SHORT).show()
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            Log.w("resultCode",resultCode.toString())
+
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
-                Log.d("idk, what I'm doing", "firebaseAuthWithGoogle:" + account.id)
+
+                Toast.makeText(baseContext, "Account: "+account.toString(), Toast.LENGTH_SHORT).show()
+
                 firebaseAuthWithGoogle(account.idToken!!)
             } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w("idk, what I'm doing", "Google sign in failed", e)
-                // ...
+                Toast.makeText(baseContext, "Google sign in failed.$resultCode", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
-        Toast.makeText(baseContext, "Authentication in process #2.",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(baseContext, "Authentication in process #2.", Toast.LENGTH_SHORT).show()
+
         val credential = GoogleAuthProvider.getCredential(idToken, null)
+
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    Log.d("idk, what I'm doing", "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUI(user)
+                    updateUI(auth.currentUser)
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("idk, what I'm doing", "signInWithCredential:failure", task.exception)
-                    // ...
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-
+                    Toast.makeText(baseContext, "Firebase failed.", Toast.LENGTH_SHORT).show()
                     updateUI(null)
                 }
-
-                // ...
             }
     }
 
