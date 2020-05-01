f505f7d118a31a7bc198dae204e3df5e10bb7a9b
dkdavid24 <dkdavid24@live.com>
2020-05-01 17:06:40 +0300

    fun
    Update email
    Delete user

--- Android/app/src/main/java/com/plasius/dailymotivationalquotes/activities/UserSettingsActivity.kt
+++ Android/app/src/main/java/com/plasius/dailymotivationalquotes/activities/UserSettingsActivity.kt
@@ -1,12 +1,21 @@
 package com.plasius.dailymotivationalquotes.activities
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 import com.plasius.dailymotivationalquotes.R
+import kotlinx.android.synthetic.main.activity_user_settings.*
+import kotlinx.android.synthetic.main.auxact_update_email.*
+import com.plasius.dailymotivationalquotes.R.string
+
+
 
 class UserSettingsActivity : AppCompatActivity() {
 
@@ -17,6 +26,8 @@ class UserSettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_settings)
 
         auth= FirebaseAuth.getInstance()
+
+        curUser.text=auth.currentUser?.email.toString()
     }
 
     fun trigLogout(view: View) {
@@ -24,5 +35,77 @@ class UserSettingsActivity : AppCompatActivity() {
         val intent= Intent(this, LoginActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
+    }
+
+    fun deleteUser(){
+        val user = auth.currentUser!!
+
+        user.delete()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext,"Success!",Toast.LENGTH_SHORT).show()
+                    val intent= Intent(this, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(baseContext, R.string.welcome, Toast.LENGTH_LONG).show()
+                    logoutButton.callOnClick()
+                }
+            }
+    }
+
+    fun trigDelete(view: View) {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(R.string.delete_user)
+            .setPositiveButton(R.string.delete_user,
+                DialogInterface.OnClickListener { dialog, id ->
+                    deleteUser()
+                })
+            .setNegativeButton(R.string.cancel,
+                DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+        // Create the AlertDialog object and return it
+        builder.create().show()
+
+
+    }
+
+    fun updateMail(){
+        val user = auth.currentUser
+
+        val newMail = nMail.text.toString()
+
+        Toast.makeText(baseContext, "Changing e-mail to: $newMail", Toast.LENGTH_SHORT).show()
+
+        user!!.updateEmail(newMail)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "Changed e-mail to: $newMail", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(baseContext, "Failed", Toast.LENGTH_SHORT).show()
+                    logoutButton.callOnClick()
+                }
+            }
+    }
+
+    fun trigUpdateMail(view: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(R.string.c_mail)
+            .setPositiveButton(R.string.save,
+                DialogInterface.OnClickListener { dialog, id ->
+                    updateMail()
+                })
+            .setNegativeButton(R.string.cancel,
+                DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+        // Create the AlertDialog object and return it
+        builder.create().show()
+
+
     }
 }
